74d28af5ccf31bd7f87c36c6e3d15e7b25c82e7d
shigure3011 <60062110+shigure3011@users.noreply.github.com>
2020-05-04 22:17:47 +0700

    Update crawler_spider.py

--- crawler/crawler/spiders/crawler_spider.py
+++ crawler/crawler/spiders/crawler_spider.py
@@ -5,19 +5,15 @@ from crawler.items import CrawlerItem
 class CrawlerSpider(Spider):
     name = 'crawler'
     allowed_domains = ["ani4u.org"]
-    start_urls = ['http://ani4u.org/xem-anime/toaru-kagaku-no-railgun-t',
-                  'http://ani4u.org/xem-anime/kaguya-sama-wa-kokurasetai-tensai-tachi-no-renai-zunousen-ss2']
-    i = 0
+    start_urls = []
+    
     def parse(self, response):
         lists = Selector(response).xpath('//div[@class="data"]')
         for anime in lists:
             item = CrawlerItem()
             item['Name'] = anime.xpath('h1/text()').extract_first()
-            genre_list = anime.xpath('p[2]/a[1]/text()').extract_first()
-            item['Genre'] = ', '.join(genre_list)
+            genre_list = anime.xpath('p[2]/a/text()').getall()
+            item['Genre'] = ', '.join(genre_list[1:])
             item['Year'] = anime.xpath('p[4]/a[2]/text()').extract_first()
-            item['View'] = anime.xpath('p[7]/text()').extract_first()
-            item['Url'] = anime.xpath('a/@href/text()').extract_first()
-            #item['Time'] = anime.xpath(
-            #    'div[@class="actionuser"]/a[@class="time"]/text()').extract_first()
+            item['Url'] = anime.xpath('a/@href').extract_first()
             yield item
